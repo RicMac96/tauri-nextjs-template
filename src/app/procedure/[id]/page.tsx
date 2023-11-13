047ed055fd27c5b48767d4890a843687048dd01c
RicMac96 <ricardo.macio@hotmail.com>
2023-11-13 09:33:46 +0000

    .

---
+++ src/app/procedure/[id]/page.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import ReportForm from "@/app/components/ReportForm"
+import { getReport, getWindfarms } from "@/utils/dataRequests"
+
+import { ReportType, WindfarmType } from "../../../../types/dataType"
+
+const Index = async ({ params }: { params: { id: string } }) => {
+  const windfarmList: WindfarmType[] | undefined = await getWindfarms()
+  const report: ReportType | undefined = await getReport(params.id)
+  if (!report || !windfarmList.length)
+    return <div>{`Results not found for ${params.id}`}</div>
+  return (
+    <ReportForm
+      windfarmList={windfarmList}
+      title="Update Report"
+      buttonText="Update"
+      report={report}
+    />
+  )
+}
+
+export default Index
--- src/app/procedure/page.tsx
+++ src/app/procedure/page.tsx
@@ -1,5 +1,12 @@
-const index = () => {
-  return <div>Porcedures</div>
-}
+import ReportForm from "@/app/components/ReportForm"
+import { getWindfarms } from "@/utils/dataRequests"
 
+import { WindfarmType } from "../../../types/dataType"
+
+const index = async () => {
+  const windfarmList: WindfarmType[] = await getWindfarms()
+  return (
+    <ReportForm windfarmList={windfarmList} title="Create Report" buttonText="Save" />
+  )
+}
 export default index
